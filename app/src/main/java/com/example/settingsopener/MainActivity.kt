f60b25eecca2a5582a6c0600b9b8a855d07216af
TheW00Xer <thew00xer@gmail.com>
2022-02-16 16:34:24 +0900

    Initial commit
    Fully working app.

---
+++ app/src/main/java/com/example/settingsopener/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.settingsopener
+
+import android.content.Intent
+import android.os.Bundle
+import android.provider.Settings
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        startActivity(Intent(Settings.ACTION_SETTINGS))
+
+    }
+}
